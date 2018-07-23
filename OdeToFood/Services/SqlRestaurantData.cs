9db9663dd6aa439ffa3243535c5ddc7ed70cdf66
Darren Rush <darrenrush@gmail.com>
2018-07-22 22:57:10 -0500

    Initial migration

---
+++ OdeToFood/Services/SqlRestaurantData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OdeToFood.Data;
+using OdeToFood.Models;
+using SQLitePCL;
+
+namespace OdeToFood.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private OdeToFoodDbContext _context;
+
+        public SqlRestaurantData(OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _context.Restaurants.OrderBy(r => r.Name);
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _context.Restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            _context.Restaurants.Add(newRestaurant);
+            _context.SaveChanges();
+            return newRestaurant;
+        }
+    }
+}
